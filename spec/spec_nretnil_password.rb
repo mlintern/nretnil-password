3bfac361507417c4a791efabfd99e60f9a1f69eb
Mark Lintern <markllintern@gmail.com>
2019-04-15 11:49:30 -0400

    do hash checks

--- spec/spec_nretnil_password.rb
+++ spec/spec_nretnil_password.rb
@@ -7,6 +7,7 @@ describe 'Nretnil::Password.character_with_symbols' do
   end
 
   it 'should produce a hash with a char and photnec equivalent' do
+    expect(Nretnil::Password.character_with_symbols.is_a?(Hash)).to be true
     expect(Nretnil::Password.character_with_symbols[:a].nil?).to be false
     expect(Nretnil::Password.character_with_symbols[:p].nil?).to be false
   end
@@ -18,6 +19,7 @@ describe 'Nretnil::Password.character_no_symbols' do
   end
 
   it 'should produce a hash with a char and photnec equivalent' do
+    expect(@character_set.is_a?(Hash)).to be true
     expect(@character_set[:a].nil?).to be false
     expect(@character_set[:p].nil?).to be false
     expect(Nretnil::Password.symbols.include?(@character_set[:a])).to be false
@@ -30,6 +32,7 @@ describe 'Nretnil::Password.get_next' do
   end
 
   it 'should produce a hash with a char and photnec equivalent' do
+    expect(Nretnil::Password.get_next.is_a?(Hash)).to be true
     expect(Nretnil::Password.get_next[:a].nil?).to be false
     expect(Nretnil::Password.get_next[:p].nil?).to be false
   end
@@ -51,16 +54,19 @@ describe 'Nretnil::Password.generate' do
   end
 
   it 'should produce 15 char password' do
+    expect(Nretnil::Password.generate.is_a?(Hash)).to be true
     expect(Nretnil::Password.generate[:password].length).to be 15
     expect(Nretnil::Password.generate[:phonetic].split(' ').length).to be 15
   end
 
   it 'generate(25) should produce 25 char password' do
+    expect(Nretnil::Password.generate(25).is_a?(Hash)).to be true
     expect(Nretnil::Password.generate(25)[:password].length).to be 25
     expect(Nretnil::Password.generate(25)[:phonetic].split(' ').length).to be 25
   end
 
   it 'generate(25, true) should produce 25 char password' do
+    expect(Nretnil::Password.generate(25, true).is_a?(Hash)).to be true
     expect(Nretnil::Password.generate(25, true)[:password].length).to be 25
     expect(Nretnil::Password.generate(25, true)[:phonetic].split(' ').length).to be 25
   end
@@ -77,6 +83,7 @@ describe 'Nretnil::Password.custom' do
     password[:password].split('').each do |char|
       expect(@custom_chars.include?(char)).to be true
     end
+    expect(Nretnil::Password.custom(10, @custom_chars).is_a?(Hash)).to be true
   end
 end
 
@@ -103,10 +110,12 @@ describe 'Nretnil::Password.phrase' do
   end
 
   it 'should produce a string' do
+    expect(Nretnil::Password.phrase.is_a?(Hash)).to be true
     expect(Nretnil::Password.phrase[:phonetic].is_a?(String)).to be true
   end
 
   it 'should have at least 4 words' do
+    expect(Nretnil::Password.phrase.is_a?(Hash)).to be true
     expect(Nretnil::Password.phrase[:phonetic].split(' ').length).to be > 3
   end
 end
